cdd380e15bfb0df48c992a271d7c7111a899150a
Madjapa <lzh1325314861@outlook.com>
2024-05-10 20:35:30 +0800

    modified:   Bupt_Hotel/urls.py 	new file:   smallHotel/urls.py

--- Bupt_Hotel/urls.py
+++ Bupt_Hotel/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    '''
+    参数1 路径名 
+    path (xxx/) 
+    '''
     path('admin/', admin.site.urls),
+    path("smallHotel/", include("smallHotel.urls")),
 ]
