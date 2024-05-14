504257665a52ea6f4a981ac69eec2765363602a2
sun-yubo1 <1819110433@qq.com>
2024-05-14 21:24:59 +0800

    modified:   Bupt_Hotel/urls.py 	new file:   smallHotel/urls.py

--- smallHotel/views.py
+++ smallHotel/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.core.files import File
 from .new import *
 # Create your views here.
@@ -27,29 +27,38 @@ def reception(request):
             rooms.append(Room(data[0], 0, 0, 0, data[1]))
 
     # 办理入住,查询空闲房间
-
-    if len(customers) < room_num:
-        for i in range(len(rooms)):
-            if int(rooms[i].done) == 0:
-                print(rooms[i].id)
-                return HttpResponse(rooms[i].id)
-    # 显示空闲的房间号,传递给前端
-    else:
-        print("你完了")
-
-    # 从前端获取顾客输入的信息
-    name = "syb"
-    room_num = "02"
     if request.method == 'GET':
-        return render(request,"reception.html",{"tips": "sssss"})
+        spare_room = ""
+        if len(customers) < room_num:
+            for i in range(len(rooms)):
+                if int(rooms[i].done) == 0:
+                    spare_room += rooms[i].id+"   "
+                    print(spare_room)
+            return render(request, "reception.html", {"tips": spare_room})
+        # 显示空闲的房间号,传递给前端
+        else:
+            return render(request, "reception.html", {"tips": "无空闲的房间"})
     if request.method == 'POST':
         name = request.POST.get('username')
         num = request.POST.get('room_num')
-        print(name,num)
-    return render(request,"reception.html")
+        for i in range(len(rooms)):
+            if num == rooms[i].id:
+                rooms[i].done = 1
+        customers.append(Customer(name,num))
+        with open("smallHotel/customers.txt", "a") as f:
+            myfile = File(f)
+            myfile.write(name + " " + num + "\n")
+        #return render(request,"reception.html",{"tips": "成功入住"})
+        return redirect("/customer")
 
 #顾客页面
 def customer(request):
+    on = request.POST.get('current_temp_button')
+    add = request.POST.get('add')
+    sub = request.POST.get('sub')
+    print(on)
+    print(sub)
+    print(add)
     return render(request, "customer.html")
 
 
